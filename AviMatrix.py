82e036511a1172b1110355c314a59085588b0647
VictorLaszlo457 <42792452+VictorLaszlo457@users.noreply.github.com>
2024-01-12 00:30:41 -0500

    Update AviMatrix.py
    Added basic cases for random matrix generation

--- AviMatrix.py
+++ AviMatrix.py
@@ -22,11 +22,36 @@ def random_matrix_generation_level(level):
 			rand_matrix = np.zeros((1, num_col))
 			# Update the values of the matrix with random integers
 			for i in range(num_col):
-				rand_matrix[0][i] = randint(1,100)
+				rand_matrix[0][i] = randint(1,25)
 	# At level 1, matrices are only small, square matrices
+	if (int(level)==1):
+		num_row = randint(1,3)
+		num_col = num_row
+		rand_matrix = np.zeros((num_row, num_col))
+		# Update the values of the matrix with random integers
+		for i in range(num_row):
+			for j in range(num_col):
+				rand_matrix[i][j] = randint(1,100)
 	# At level 2, matrices are large square matrices
+	if (int(level)==2):
+		num_row = randint(3,6)
+		num_col = num_row
+		rand_matrix = np.zeros((num_row, num_col))
+		# Update the values of the matrix with random integers
+		for i in range(num_row):
+			for j in range(num_col):
+				rand_matrix[i][j] = randint(1,100)
 	# At level 3, matrices may be differing dimensions of multiplication and just larger for addition and subtraction
+	if (int(level)==3):
+		num_row = randint(1,6)
+		num_col = randint(1,6)
+		rand_matrix = np.zeros((num_row, num_col))
+		# Update the values of the matrix with random integers
+		for i in range(num_row):
+			for j in range(num_col):
+				rand_matrix[i][j] = randint(1,100)
 	print(rand_matrix)
+	print(rand_matrix.shape)
 	return rand_matrix
 
 
@@ -35,7 +60,8 @@ class AviMatrix:
 	matrix = np.zeros((0,0))
 
 	def main():
-		random_matrix_generation_level(0) 
+		#random_matrix_generation_level(2) 
+		random_matrix_generation_level(3) 
 	
 	if __name__ == "__main__":
 		main()
