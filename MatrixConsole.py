c41fbcbca0e942971ff0d73628e7b4ac2cf951cb
VictorLaszlo457 <42792452+VictorLaszlo457@users.noreply.github.com>
2024-01-11 23:23:17 -0500

    Update MatrixConsole.py
    Basic description of tool's purpose

--- MatrixConsole.py
+++ MatrixConsole.py
@@ -1,1 +1,12 @@
-class MatrixConsole:
+class Matrix_Console: 
+    def main():
+        print("The Smith Matrix Simulator 2.0 was developed to help students practice elementary matrix manipulation skills that they will employ frequently during their pursuit in their studies of computer science, mathematics, engineering or related fields. This version of the calculator is equip with basic tools such as tutorials, a matrix calculator, a practice problem generator, and timed labs")
+        print("If you have questions or need a reminder on how to do some of the functions click the \"tutorials\" button to see an example of how to work different types of problems")
+        print("If you want to check your work on an already completed problem, the \"matrix calculator\" is  a great tool.")
+        print("Practice makes perfect! If you want to work on some extra problems, the \"practice problems generator\" has three difficulty settings so you can improve at your own pace.")
+        print("Want to challenge yourself and get ready for the time constraints of your next big exam? Click the \"timed lab\", where you can set the timer, number of problems, difficulty, and save your performance.") 
+        print("Track your progress! Compete with your friends! Earn the title of Supreme Matrix Master!")
+        print("...Or just learn a new thing or two while having fun! :)")           
+
+    if __name__ == "__main__":
+        main()
